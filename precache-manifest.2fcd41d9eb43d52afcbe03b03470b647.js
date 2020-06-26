c45b7c26d3512d2ee3e82e0a579c1f721a40f3d0
Gorm <gdsoucie@gmail.com>
2020-06-25 23:31:19 -0400

    Updates

---
+++ precache-manifest.2fcd41d9eb43d52afcbe03b03470b647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b845254036e12115a74582fc53c74dc",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/index.html"
+  },
+  {
+    "revision": "b6244909bfe211e31914",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/static/css/main.a63bf79a.chunk.css"
+  },
+  {
+    "revision": "dd43f42d97ce4a02fcd7",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/static/js/2.2421776a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/static/js/2.2421776a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6244909bfe211e31914",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/static/js/main.2473acde.chunk.js"
+  },
+  {
+    "revision": "274fcabb34bb67de812a",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/static/js/runtime-main.8e1a0442.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GormTheWyrm/BCReactEmplyeeDirectory/static/media/logo.5d5d9eef.svg"
+  }
+]);
