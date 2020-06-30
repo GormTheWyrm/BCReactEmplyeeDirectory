79574fb36ec9f9c92f4e1bce6cae2c9508a14162
Gorm <gdsoucie@gmail.com>
2020-06-30 15:12:36 -0400

    changed homepage

--- src/components/OptionsTab.js
+++ src/components/OptionsTab.js
@@ -7,27 +7,26 @@ function OptionsTab(props) {
         <div>
             <div className="row">
                 <div className={"col-12"}>
-                    <p>Instructions: select a value to sort and a value to filter by. </p>
+                    <h5>Instructions:</h5> 
+                     <p>Select a value to sort and a value to filter by. </p>
                     <p>Currently Filtered By: {props.filter} department{props.filter === "all" ? "s" : ""} </p>
                     <p>Currently Sorted By: {props.sort}</p>
                 </div>
             </div>
             <div className="row">
                 <div className="col-6">
-                    <select className="custom-select" value={props.filter} onChange={props.setFilter}>
-                        
-                        {/* this calls SetFilter*/}
-                        <option defaultValue="all">Sort By: all </option>
+                    <select className="custom-select" value={props.filter} onChange={props.setFilter}>                       
+                        <option value="all">Filter By: All</option>
                         <option value="Propaganda">Filter By: Propaganda Department</option>
                         <option value="Executive">Filter By: Executive Department </option>
                         <option value="Acquisitions">Filter By: Acquisitions Department</option>
                         <option value="Intelligence">Filter By: Intelligence Department</option>
-                        <option value="all">Filter By: All</option>
+                        
                     </select>
                 </div>
                 <div className="col-6">
-                    <select className="custom-select">
-                        <option defaultValue="default">Sort By: default </option>
+                    <select className="custom-select" value={props.sort} onChange={props.setSort}>
+                        <option value="default">Sort By: default </option>
                         <option value="id">Sort By: Id </option>
                         <option value="firstName">Sort By: First Name</option>
                         <option value="lastName">Sort By: Last Name</option>
--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -19,156 +19,98 @@ class Directory extends React.Component {
         let myFilter = event.target.value;
         console.log(myFilter);
         this.setState({ filter: myFilter });
+        let newArray = EmployeeList.filter(emp => emp.department === myFilter);
+        if (myFilter === "all") {
+            this.setState({ viewList: EmployeeList });    //Worked!
+        } else {
+            this.setState({ viewList: newArray });    //did not work!
+        }
+    }
 
-        let newArray= EmployeeList.filter(emp => emp.department === myFilter);  
-   
-        // let newArray = EmployeeList.filter(function (emp) {
-        //     if (emp.department === myFilter){
-        //         return emp;
-        //         }       
-        // });
+    setSort = (event) => {
+        console.log("setSort function");
+        let mySort = event.target.value;
+        console.log(mySort);
+        let sortingArray = this.state.viewList;
+        // sortingArray.sort(function = (a,b) => {});
+        sortingArray.sort(function(a,b) {
+            //a.mySort  // that wont work, will it?
+            
+        });
 
+        // let sortedArray = this.state.viewList.sort();   //testing; not a good function!
+        // I need to sort objects by a property...
+        // let sortedArray = this.state.viewList.sort();
 
-    if(myFilter === "all") {
-    this.setState({ viewList: EmployeeList });    //Worked!
-} else {
-    this.setState({ viewList: newArray });    //did not work!
-}
+        // console.log(sortedArray);
 
-
+        /*
+        array.sort(compareFunction)
+        
+        function(a, b){return a-b}
+        When the sort() method compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
+        
+        Example:
+        
+        When comparing 40 and 100, the sort() method calls the compare function(40,100).
+        
+        The function calculates 40-100, and returns -60 (a negative value).
+        
+        The sort function will sort 40 as a value lower than 100.
+        */
 
     }
 
-setSort = (event) => {
-    console.log("setSort function");
-    let mySort;
 
 
 
-}
+    // componentDidMount() {
 
+    //     console.log("mounted");
 
+    // }
+    // componentDidUpdate() {
 
-
-
-
-
-
+    //     console.log("component did update")
+    // }
 
 
 
 
 
-filterTable() {
-    let myFilter = "Propaganda";   //change this to this.state.filter after testing!
-    //need to change this to in input or dropdown!
-    if (this.state.filter === "all") {
-        //set viewList to EmployeeList
-        this.setState({ viewList: EmployeeList })
-    } else {
-        //set view list to a filtered array
-        //will need to change this so that it displays different options
-        this.setState({ viewList: EmployeeList.filter(emp => emp.department === myFilter) })
-        console.log(EmployeeList.filter(emp => emp.department === myFilter));
-        console.log("sort table");
-        // need to replace the "test" with an input value for the department name...
+
+    render() {
+        return (
+            <div>
+                <OptionsTab
+                    setFilter={this.setFilter}
+                    setSort={this.setSort}
+                    filter={this.state.filter}
+                    sort={this.state.sort}
+                />
+                <table className="table table-dark">
+                    <thead>
+                        <TableHead />
+                    </thead>
+                    <tbody>
+                        {this.state.viewList.map(employee => (
+                            <TableRow
+                                key={employee.id}
+                                myId={employee.id}
+                                firstName={employee.firstName}
+                                lastName={employee.lastName}
+                                department={employee.department}
+                                role={employee.role}
+                                salary={employee.salary}
+                            />
+                        ))}
+
+                    </tbody>
+                    {/* <p>TestVar {this.state.testVar}</p> */}
+                </table>
+            </div>
+        );
+
     }
-
-}   //havign issues because its async...
-
-sortTable() {
-
-
-    let mySort = "first name";
-
-    this.setState({ sort: mySort });
-    // this.render();
-}   //this is not causing rerender...
-
-
-componentDidMount() {
-    //runs after output is rendered to DOM...
-    //so put anything that needs to function each time compenent is rendered here
-    console.log("mounted");
-    // console.log(EmployeeList);
-}
-componentDidUpdate() {
-    //must have a condition if set state here
-    console.log("component did update")
-}
-
-
-handleChange(event) {
-    const target = event.target;
-    const value = target.name === "isGoing" ? target.checked : target.value;
-    //need to figure out above line https://reactjs.org/docs/forms.html
-    const name = target.name;
-    this.setState({
-        [name]: value
-
-    });
-}
-handleSubmit(event) {
-    // not sure if these are standard or if I need to make em 
-    event.preventDefault();
-    //use onSubmit={this.handleSubmit}
-}
-
-handleTest = event => {
-    console.log("temp button!");
-    // this.setState({ filter: "Propaganda" });
-    // this.filterTable(event);
-    this.setFilter(event);
-    console.log(event.target.value);
-}
-
-
-render() {
-    return (
-        <div>
-
-
-
-            <button type="button" className="btn btn-primary"
-                data-toggle="button" aria-pressed="false"
-                onClick={this.handleTest}>
-                Single toggle - temp button
-                </button>
-
-            {/* temporary! */}
-
-
-
-            <OptionsTab
-                // method 1: filter
-                setFilter={this.setFilter}
-                // method 2: sort
-                filter={this.state.filter}
-                sort={this.state.sort}
-            />
-            <table className="table table-dark">
-                <thead>
-                    <TableHead />
-                </thead>
-                <tbody>
-                    {this.state.viewList.map(employee => (
-                        <TableRow
-                            key={employee.id}
-                            myId={employee.id}
-                            firstName={employee.firstName}
-                            lastName={employee.lastName}
-                            department={employee.department}
-                            role={employee.role}
-                            salary={employee.salary}
-                        />
-                    ))}
-
-                </tbody>
-                {/* <p>TestVar {this.state.testVar}</p> */}
-            </table>
-        </div>
-    );
-
-}
 }
 export default Directory;
